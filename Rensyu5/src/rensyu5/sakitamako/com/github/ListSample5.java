87b569d206040b54a5c2ea23aefaefa93aaa62b0
sakitamako <sa.919.1025.ki@gmail.com>
2024-03-27 21:42:15 +0900

    編集

--- Rensyu5/src/rensyu5/sakitamako/com/github/ListSample5.java
+++ Rensyu5/src/rensyu5/sakitamako/com/github/ListSample5.java
@@ -7,7 +7,7 @@ public class ListSample5 {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
 		
-		//値の記憶は add メソッドを利用
+		//値の記憶は add メソッドを利用する
 	    list.add("1");
 	    list.add("2");
 	    list.add("3");
@@ -20,7 +20,7 @@ public class ListSample5 {
 	    System.out.println(list.get(i));
 	    }
 	    
-	    //拡張 for 文を利用するともっと簡単
+	    //拡張 for 文を利用するともっと簡単です
 	    for (String s: list) {
 	    	System.out.println(s);
 	    }
